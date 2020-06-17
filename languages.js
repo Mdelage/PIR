6a43f4042cd8a3b76e74d0eaa6fb972dffa79c10
Martin Delage <m.delage204@protonmail.com>
2020-06-17 10:24:47 +0200

    Changed the pre-determined messages

--- languages.js
+++ languages.js
@@ -27,35 +27,8 @@ Three types exist for each game type:
   french9 : () => { return "L'autre joueur veut vous donner de l'énergie." },
   french10 : () => { return "L'autre joueur veut de l'énergie." } */
 
-/* The messages here pull information instead of pushing it. */
-const messages0 = {
-
-  english1 : () => { return "Go fill the water tank." },
-  english2 : () => { return "I'm going to refuel my water." },
-  english3 : () => { return "Do you need water?" },
-  english4 : () => { return "Do you need energy?" },
-  english5 : () => { return "Yes" },
-  english6 : () => { return "No" },
-  english7 : () => { return "The other player is trying to give you water." },
-  english8 : () => { return "The other player wants water." },
-  english9 : () => { return "The other player is trying to give you energy." },
-  english10 : () => { return "The other player wants energy." },
-
-  french1 : () => { return "Va remplir le réservoir d'eau." },
-  french2 : () => { return "Je vais recharger mon eau." },
-  french3 : () => { return "As-tu besoin d'eau ?" },
-  french4 : () => { return "As-tu besoin d'énergie ?" },
-  french5 : () => { return "Oui" },
-  french6 : () => { return "Non" },
-  french7 : () => { return "L'autre joueur veut vous donner de l'eau." },
-  french8 : () => { return "L'autre joueur veut de l'eau." },
-  french9 : () => { return "L'autre joueur veut vous donner de l'énergie." },
-  french10 : () => { return "L'autre joueur veut de l'énergie." }
-}
-exports.messages0 = messages0;
-
 /* The messages here push information instead of pulling it. */
-const messages1 = {
+const messages0 = {
 
   english1 : () => { return "I'm filling the water tank." },
   english2 : () => { return "I'm going to refuel my water." },
@@ -72,6 +45,33 @@ const messages1 = {
   french2 : () => { return "Je vais recharger mon eau." },
   french3 : () => { return "J'ai besoin d'eau." },
   french4 : () => { return "J'ai besoin d'énergie." },
+  french5 : () => { return "Oui" },
+  french6 : () => { return "Non" },
+  french7 : () => { return "L'autre joueur veut vous donner de l'eau." },
+  french8 : () => { return "L'autre joueur veut de l'eau." },
+  french9 : () => { return "L'autre joueur veut vous donner de l'énergie." },
+  french10 : () => { return "L'autre joueur veut de l'énergie." }
+}
+exports.messages0 = messages0;
+
+/* The messages here pull information instead of pushing it. */
+const messages1 = {
+
+  english1 : () => { return "Can you feel the water tank?" },
+  english2 : () => { return "I'm going to refuel my water." },
+  english3 : () => { return "Do you need water?" },
+  english4 : () => { return "Do you need energy?" },
+  english5 : () => { return "Yes" },
+  english6 : () => { return "No" },
+  english7 : () => { return "The other player is trying to give you water." },
+  english8 : () => { return "The other player wants water." },
+  english9 : () => { return "The other player is trying to give you energy." },
+  english10 : () => { return "The other player wants energy." },
+
+  french1 : () => { return "Peux-tu remplir le réservoir ?" },
+  french2 : () => { return "Je vais recharger mon eau." },
+  french3 : () => { return "As-tu besoin d'eau ?" },
+  french4 : () => { return "As-tu besoin d'énergie ?" },
   french5 : () => { return "Oui" },
   french6 : () => { return "Non" },
   french7 : () => { return "L'autre joueur veut vous donner de l'eau." },
